6893d8b1eace6f02f9680c1d53666c31603b9425
Miki Ashkenazi <mikiash@gmail.com>
2020-10-07 16:42:27 +0300

    app.component grid skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
+import { HeaderComponent } from './layout/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBV2-GsduK6TrIyIbHWQLsMXK72EUslwHg",
+    authDomain: "fairil-aae18.firebaseapp.com",
+    databaseURL: "https://fairil-aae18.firebaseio.com",
+    projectId: "fairil-aae18",
+    storageBucket: "fairil-aae18.appspot.com",
+    messagingSenderId: "11082823879",
+    appId: "1:11082823879:web:e0dd3fc59c321b994fe6e2",
+    measurementId: "G-P1DELX8533"
+  }
 };
