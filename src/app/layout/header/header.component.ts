d17357b1eafa3fcd5303be2ff32d4c93b2b506e2
Miki Ashkenazi <mikiash@gmail.com>
2020-10-24 15:42:25 +0300

    new store dialog pop action

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
+import { NewStoreComponent } from '@app/dialogs/new-store/new-store.component';
 
 @Component({
   selector: 'yrd-header',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {
+  }
+
+  popDialog(): void {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '640px';
+    dialogConfig.height = '800px';
+
+    const dialogRef = this.dialog.open(NewStoreComponent, dialogConfig);
+    // dialogRef.afterClosed().subscribe(result => {
+    //   console.log(`Dialog result: ${result}`);
+    // });
   }
 
 }
