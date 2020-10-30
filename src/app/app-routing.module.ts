b19111b74111ae902e9e47c7be20ceb2326bd7bf
Miki Ashkenazi <mikiash@gmail.com>
2020-10-30 11:48:11 +0200

    first basic route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StoresComponent } from 'app/stores/stores.component';
+import { SingleStoreComponent } from 'app/single-store/single-store.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/stores', pathMatch: 'full' },
+  { path: 'stores', component: StoresComponent },
+  { path: 'store', component: SingleStoreComponent },
+  // { path: 'store/:id', component: SingleStoreComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
