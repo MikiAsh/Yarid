acbd1f9965c7dc898caedfe62eeccf34e327c328
Miki Ashkenazi <mikiash@gmail.com>
2020-10-30 11:47:10 +0200

    single-store.component

---
+++ src/app/single-store/single-store.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'yrd-single-store',
+  templateUrl: './single-store.component.html',
+  styleUrls: ['./single-store.component.scss']
+})
+export class SingleStoreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
