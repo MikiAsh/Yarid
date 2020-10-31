bd4a0a2e6d2a3a61696d206396be4df247d3c95d
Miki Ashkenazi <mikiash@gmail.com>
2020-10-31 16:04:03 +0200

    get single store

--- src/app/single-store/single-store.component.ts
+++ src/app/single-store/single-store.component.ts
@@ -1,20 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { StoresService } from '../stores/stores.service';
 import { Store } from '@models/store';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'yrd-single-store',
   templateUrl: './single-store.component.html',
   styleUrls: ['./single-store.component.scss']
 })
-export class SingleStoreComponent implements OnInit {
-
-  constructor(private db: StoresService, private route: ActivatedRoute) {}
+export class SingleStoreComponent implements OnInit, OnDestroy {
+  store: Store;
+  sub: Subscription;
+  constructor(private storesService: StoresService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     const storeId = this.route.snapshot.paramMap.get('id');
-    console.log('route id', storeId);
+    this.sub = this.storesService.getStore(storeId).subscribe(data => {
+      this.store = data; 
+      this.storesService.selectedStore$.next(data)
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 
 }
--- src/app/stores/stores.service.ts
+++ src/app/stores/stores.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Store } from '@models/store';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StoresService {
+  selectedStore$ = new BehaviorSubject<Store>(null);
+
   constructor(private db: AngularFirestore) {}
   
   getStores(): Observable<Store[]> {
     return this.db.collection('stores').valueChanges({ idField: 'id' }) as Observable<Store[]>;
+  }
+
+  getStore(id: string): Observable<Store> {
+    return this.db.collection('stores').doc(id).valueChanges() as Observable<Store>;
   }
 
   createStore(store: Store) {
