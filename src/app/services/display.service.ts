e17deb4fc4a5918401679a5033d46a2b68922c02
Miki Ashkenazi <mikiash@gmail.com>
2020-11-03 15:36:49 +0200

    added banner$ Subject to DisplayService

--- src/app/services/display.service.ts
+++ src/app/services/display.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { MenuEntry } from '@models/menu-entry';
 import { BehaviorSubject } from 'rxjs';
+import { Store } from '@models/store';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DisplayService {
   menu$ = new BehaviorSubject<MenuEntry[]>(null);
+  banner$ = new BehaviorSubject<Store>(null);
   constructor() { }
 }
