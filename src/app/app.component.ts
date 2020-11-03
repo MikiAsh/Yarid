f842b488a1aadfab2ce402c6cdf0e7fff2250f42
Miki Ashkenazi <mikiash@gmail.com>
2020-11-03 09:06:24 +0200

    undo app.component populate nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { navData } from '@config/nav-menu';
-import { MenuEntry } from "@app/models/menu-entry";
 import { StoresService } from '@app/stores/stores.service';
 import { Subscription } from 'rxjs';
 
@@ -9,28 +7,14 @@ import { Subscription } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit {
   title = 'Yarid';
-  menuEntries: MenuEntry[] = navData;
   sub: Subscription;
+  
   constructor(private storesService: StoresService) {}
 
   ngOnInit(): void {
-    this.populateNavMenu();
+
   }
 
-  populateNavMenu(): void {
-    this.sub = this.storesService.selectedStore$.subscribe(store => {
-      if (store && store.tags) {
-        this.menuEntries = store.tags.map(tag => ({displayName: tag}));
-      }
-      else {
-        this.menuEntries = [];
-      }
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
-  }
 }
